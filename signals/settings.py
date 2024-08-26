b596520d4cef4d0fd6b6bb4623c34d56029dc491
fastestFingers <oluwatimilehinseriki@gmail.com>
2024-08-26 22:42:52 +0100

    update

--- signals/settings.py
+++ signals/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 ALLOWED_HOSTS = [
-    "localhost",
+    "127.0.0.1",
     # "relltrader.com",
     # "www.relltrader.com",
     #  "86.48.6.77"
